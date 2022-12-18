86c6f1d7be24940d65e1ecf1256df0c5a9fb4d9a
Howard Yen <hwrdpvt@gmail.com>
2022-12-17 20:31:46 -0500

    Second draft of brainflix-api server

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ var cors = require("cors");
 const app = express();
 const videoRoutes = require('./routes/videos');
 
+//configuration
+require('dotenv').config();
+const PORT = process.env.PORT || 8080;
+
+//middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+
 app.use("/", videoRoutes);
 
 app.listen(8080, function() {
